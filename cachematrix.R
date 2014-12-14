5ba4516dcf5b1ec1e3df5693116ff2b1f0f7e0c8
Anil Yasin <anilyasin@icloud.com>
2014-12-14 15:51:20 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,12 @@
-MakeCacheMatriks<- function(A = matrix()) {
+AYAR<-MakeCacheMatriks()
+
+MakeCacheMatriks<- function(A = matrix()) { 
         m<-NULL
-        set<-function(AR){
-                A<<-AR
-                m<<-NULL
+        set<-function(AR){ 
+                A<<-AR 
+                m<<-NULL 
         }
-        get<-function() A
+        get<-function() 
         setmatrix<-function(solve) m<<- solve
         getmatrix<-function() m
         list(set=set, get=get, setmatrix=setmatrix, getmatrix=getmatrix)
@@ -13,7 +15,7 @@ MakeCacheMatriks<- function(A = matrix()) {
 CacheSolve <- function(A=matrix(), ...) {
         m<-A$getmatrix()
         if(!is.null(m)){
-                message("getting cached data")
+                message("getting cached data") 
                 return(m)
         }
         matrix<-A$get()
